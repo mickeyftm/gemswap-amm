386b35383f6322507f77ddf0d84f2c7101c78701
DecentralisedTech <contact@decentralised.tech>
2021-07-11 16:24:51 +1200

    Cleanup unused vars for prod

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,5 +1,5 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
-import { save, load } from 'redux-localstorage-simple'
+import { configureStore } from '@reduxjs/toolkit'
+import { load } from 'redux-localstorage-simple'
 
 import application from './application/reducer'
 import { updateVersion } from './global/actions'
